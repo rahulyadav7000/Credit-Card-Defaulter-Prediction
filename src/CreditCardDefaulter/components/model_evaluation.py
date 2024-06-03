b1ef552acae419aeb6ba73a54b443e8536a0d220
rahul yadav <rahul.yadavv7000@gmail.com>
2024-06-03 21:32:13 +0530

    update

---
+++ src/CreditCardDefaulter/components/model_evaluation.py
@@ -0,0 +1,59 @@
+import os
+import sys
+import mlflow
+import mlflow.sklearn
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score, roc_auc_score
+from urllib.parse import urlparse
+from dataclasses import dataclass
+
+from src.CreditCardDefaulter.logger import logging
+from src.CreditCardDefaulter.exception import CustomException
+from src.CreditCardDefaulter.utils.utils import load_object
+import src.CreditCardDefaulter.utils.mlflow_setup as mlflow_setup
+
+
+class ModelEvaluation:
+    def __init__(self):
+        pass
+
+    def eval_metrics(self, model, features, label):
+        accuracy = round(cross_val_score(model, features, label, cv=10, n_jobs=-1, scoring='accuracy').mean(), 2)
+        f1 = round(cross_val_score(model, features, label, cv=10, n_jobs=-1, scoring='f1').mean(), 2)
+        precision = round(cross_val_score(model, features, label, cv=10, n_jobs=-1, scoring='precision').mean(), 2)
+        recall = round(cross_val_score(model, features, label, cv=10, n_jobs=-1, scoring='recall').mean(), 2)
+        roc_auc = round(cross_val_score(model, features, label, cv=10, n_jobs=-1, scoring='roc_auc').mean(), 2)
+        return accuracy, f1, precision, recall, roc_auc
+
+    def initiate_model_evaluation(self, train_array, test_array):
+        try:
+            X_test, y_test = test_array[:, :-1], test_array[:, -1]
+            model_path = os.path.join("artifacts", "model.pkl")
+            model = load_object(model_path)
+
+            mlflow.set_registry_uri("https://dagshub.com/rahulyadav7000/Credit-Card-Defaulter-Prediction.mlflow")
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme        
+            print(tracking_url_type_store)
+
+            with mlflow.start_run():
+                mlflow.set_tag("Best Model", str(model).split("(")[0])
+                
+                accuracy, f1, precision, recall, roc_auc = self.eval_metrics(model, X_test, y_test)
+
+                logging.info("accuracy_score: {}".format(accuracy))
+                logging.info("f1_score: {}".format(f1))
+                logging.info("precision_score: {}".format(precision))
+                logging.info("recall_score: {}".format(recall))
+                logging.info("roc_auc_score: {}".format(roc_auc))
+
+                mlflow.log_metric("accuracy score", accuracy)
+                mlflow.log_metric("f1 score", f1)
+                mlflow.log_metric("precision score", precision)
+                mlflow.log_metric("recall score", recall)
+                mlflow.log_metric("roc_auc score", roc_auc)
+                mlflow.sklearn.log_model(model, "model")
+                
+        except Exception as e:
+            raise CustomException(e, sys)
--- src/CreditCardDefaulter/pipelines/training_pipeline.py
+++ src/CreditCardDefaulter/pipelines/training_pipeline.py
@@ -7,11 +7,12 @@ from src.CreditCardDefaulter.exception import CustomException
 import pandas as pd
 
 from src.CreditCardDefaulter.components.data_ingestion import DataIngestion
+from src.CreditCardDefaulter.components.model_evaluation import ModelEvaluation
 
 
 obj = DataIngestion()
-
 train_data_path, test_data_path = obj.initiate_data_ingestion()
+
 data_transformation = DataTransformation()
 train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
 
@@ -21,3 +22,4 @@ best_model, best_model_score = model_trainer.initiate_model_training(train_arr, 
 
 model_eval_obj = ModelEvaluation()
 model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
+
